da67c55e8a356795cf792ab03ffc21e486a9d217
PotatoPeng <346168964@qq.com>
2023-03-26 23:40:49 +0800

    first code

---
+++ helloyocto/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf ("Hello Yocto!\n");
+	printf ("I am helloyocto project\n");
+	
+    return 0;
+} /* ----- End of main() ----- */
+
+
